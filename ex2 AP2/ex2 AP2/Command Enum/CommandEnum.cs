81785bc49eb234faf4711f9a3703ac6861acb77d
efiso <efisofer26@gmail.com>
2018-05-05 20:47:10 +0300

    added a title to the settings window

---
+++ ex2 AP2/ex2 AP2/Command Enum/CommandEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex2_AP2.Command_Enum
+{
+    public enum CommandEnum : int
+    {
+        NewFileCommand,
+        GetConfigCommand,
+        LogCommand,
+        CloseCommand
+    }
+}
