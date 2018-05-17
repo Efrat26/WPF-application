fca5ff39e940c2edec5c674a97c196b5544cacc6
efiso <efisofer26@gmail.com>
2018-05-17 17:30:37 +0300

    changed locations of more files

---
+++ ex2 AP2/Infrastructure/AppConfigObjects/ImageServiceAppConfigItem.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logs.AppConfigObjects
+{
+    public class ImageServiceAppConfigItem
+    {
+        private String outputFolder;
+        public String OutputFolder { get { return this.outputFolder; } set { this.outputFolder = value; } }
+        private String handlers;
+        public String Handlers { get { return this.handlers; } set { this.handlers = value; } }
+        private String sourceName;
+        public String SourceName { get { return this.sourceName; } set { this.sourceName = value; } }
+        private String logName;
+        public String LogName { get { return this.logName; } set { this.logName = value; } }
+        private int thumbnailSize;
+        public int ThumbnailSize{ get { return this.thumbnailSize; } set { this.thumbnailSize = value; } }
+        public ImageServiceAppConfigItem(String output, String handlers, String source,
+            String log, int size)
+        {
+            this.outputFolder = output;
+            this.sourceName = source;
+            this.logName = log;
+            this.handlers = handlers;
+            this.thumbnailSize = size;
+        }
+        public string ToJSON()
+        {
+            JObject appConfigItem = new JObject();
+            appConfigItem["output"] = this.outputFolder;
+            appConfigItem["source"] = this.sourceName;
+            appConfigItem["log"] = this.logName;
+            appConfigItem["handlers"] = this.handlers;
+            appConfigItem["size"] = this.thumbnailSize;
+            return appConfigItem.ToString();
+        }
+        public static ImageServiceAppConfigItem FromJSON(string str)
+        {
+            ImageServiceAppConfigItem appconf = new ImageServiceAppConfigItem(null,null,null,null,0);
+            JObject appconfItem = JObject.Parse(str);
+            appconf.outputFolder = (string)appconfItem["output"];
+            appconf.sourceName = (string)appconfItem["source"];
+            appconf.logName = (string)appconfItem["log"];
+            appconf.handlers = (string)appconfItem["handlers"];
+            appconf.thumbnailSize = (int)appconfItem["size"];
+            return appconf;
+        }
+    }
+}
---
+++ ex2 AP2/Infrastructure/HandlerToClose.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logs.Controller.Handlers
+{
+    public class HandlerToClose
+    {
+        private string path;
+        public string Path { get { return this.path; } set { this.path = value; } }
+        public HandlerToClose(String p)
+        {
+            this.path = p;
+        }
+        public string ToJSON()
+        {
+            JObject appConfigItem = new JObject();
+            appConfigItem["path"] = this.path;
+            return appConfigItem.ToString();
+        }
+        public static HandlerToClose FromJSON(string str)
+        {
+            HandlerToClose handlerObj = new HandlerToClose(null);
+            JObject handlerObjItem = JObject.Parse(str);
+            handlerObj.path = (string)handlerObjItem["path"];
+            return handlerObj;
+        }
+    }
+}
